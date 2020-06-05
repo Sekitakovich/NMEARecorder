4c6ed1cc5f9b8a3cec78117506a35f4a4aa0610d
K.Seki <k-seki@klabo.co.jp>
2020-06-05 12:40:18 +0900

    1st commit

--- main.py
+++ main.py
@@ -3,79 +3,146 @@ import serial
 from typing import List
 from dataclasses import dataclass
 from datetime import datetime as dt
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue as MPQueue
+from threading import Thread, Lock
+from queue import Queue, Empty
+from contextlib import closing
+from loguru import logger
 
 
 @dataclass()
 class SerialPort(object):
-
     name: str
     baudrate: int
 
 
-class Receiver(Process):
+@dataclass()
+class Record(object):
+    sentence: bytes  # NMEA asis
+    passed: float  # delta secs from prev
+    at: dt  # 受信日時
+
+
+class Receiver(Thread):
 
     def __init__(self, *, port: str, baudrate: int, qp: Queue):
 
         super().__init__()
-
         self.daemon = True
-        self.isReady: bool = False
-        self.qp: Queue = qp
 
+        self.port = port
+        self.baudrate = baudrate
+
+        self.isReady = False
+        self.qp = qp
         try:
-            sp = serial.Serial(port=port, baudrate=baudrate)
-        except serial.SerialException as e:
-            print(e)
+            self.sp = serial.Serial(port=port, baudrate=baudrate)
+        except (OSError,) as e:
+            logger.error(e)
         else:
             self.isReady = True
-            self.sp = sp
+
+    def run(self) -> None:
+        try:
+            while self.isReady:
+                data = self.sp.readline()
+                self.qp.put(data)
+        except (serial.SerialException, OSError) as e:
+            self.isReady = False
+            logger.error(e)
+
+    def __del__(self):
+        if self.isReady:
+            self.sp.close()
+            logger.info('%s was closed' % self.port)
+
+
+class DBSession(Process):
+    def __init__(self, *, qp: MPQueue):
+        super().__init__()
+        self.daemon = True
+        self.qp = qp
 
     def run(self) -> None:
         while True:
-            data = self.sp.readline()
-            self.qp.put(data)
-
-    def __del__(self):
-        if self.sp:
-            self.sp.close()
+            rows: List[Record] = self.qp.get()
+            now = dt.now()
+            dbPath = 'db'
+            dbfile: str = '%s/%04d-%02d-%02d.db' % \
+                          (dbPath,
+                           now.year, now.month, now.day)
+            print(rows)
 
 
 class Main(object):
 
     def __init__(self, *, port: List[SerialPort]):
 
-        self.device: List[Process] = []
-        self.counter: int = 0
-        self.qp: Queue = Queue()
-
+        self.ready: bool = True
+        self.locker = Lock()
         now = dt.now()
-        self.dbPath = 'db'
-        self.dbfile: str = '%s/%04d-%02d-%02d-%02d-%02d-%02d.db' % \
-                           (self.dbPath,
-                            now.year, now.month, now.day,
-                            now.hour, now.minute, now.second)
+
+        self.device: List[Thread] = []
+
+        self.counter: int = 0
+        self.lastat: dt = now
+        self.timeout: int = 5
+        self.qp: Queue = Queue()
+        self.buffer: List[Record] = []
+        self.full: int = 64
+        self.lastsave: dt = now
+
+        self.saveQueue = MPQueue()
+        self.dbsession = DBSession(qp=self.saveQueue)
+        self.dbsession.start()
 
         for s in port:
             t = Receiver(port=s.name, baudrate=s.baudrate, qp=self.qp)
-            if t.isReady:
+            try:
                 t.start()
-                self.device.append(t)
+            except Exception as e:
+                logger.error(e)
             else:
-                pass
+                if t.isReady:
+                    self.device.append(t)
+                else:
+                    self.ready = False
+                    break
 
-        print(self.device)
+    def save(self):
+        if len(self.buffer):
+            with self.locker:
+                now = dt.now()
+                rows = self.buffer.copy()
+                self.saveQueue.put(rows)
+                logger.debug('+++ saved %d rows after %f' % (len(rows), (now-self.lastsave).total_seconds()))
+                self.buffer.clear()
+                self.lastsave = now
+
+    def start(self):
         while True:
             try:
-                raw: bytes = self.qp.get()
-            except (KeyboardInterrupt, serial.SerialException) as e:
-                print(e)
+                raw: bytes = self.qp.get(timeout=self.timeout)
+            except Empty as e:
+                # logger.debug('Timeout')
+                self.save()
+            except (KeyboardInterrupt,) as e:
+                self.save()
+                logger.error(e)
                 break
             else:
                 now = dt.now()
-                nmea = raw.decode()
-                print('at %s: %s' % (now, raw))
+                record = Record(sentence=raw, at=now, passed=(now - self.lastat).total_seconds())
+
+                with self.locker:
+                    self.buffer.append(record)
+                if len(self.buffer) == self.full:
+                    # logger.debug('Full')
+                    self.save()
+
+                # print(record)
                 self.counter += 1
+                self.lastat = now
 
 
 if __name__ == '__main__':
@@ -94,8 +161,9 @@ if __name__ == '__main__':
             baudrate: int = int(ooo[1])
             port.append(SerialPort(name=name, baudrate=baudrate))
     except (IndexError, ValueError) as e:
-        print(e)
+        logger.error(e)
     else:
-        print(port)
         if len(port):
             main = Main(port=port)
+            if main.ready:
+                main.start()
